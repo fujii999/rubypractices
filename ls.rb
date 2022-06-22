f38737484eac974e6497828e4c34f6529e4fdfc7
fujii <fujii@qinc.co.jp>
2022-06-22 11:43:28 +0900

    取得するファイルが大きくなった時の挙動を変更

--- ls.rb
+++ ls.rb
@@ -8,10 +8,10 @@ def files_array
   all_files = all_files_directory
   div_filecount = (all_files.size / COLUMN.to_f).ceil(0) # 縦何行まで表示するか
   array_files = all_files.each_slice(div_filecount).to_a # 必要行数で分割
-  if array_files.size == 1
+  if array_files.size == 1 #ファイルが一つの時の挙動
     array_files
   else
-    array_files.inject(&:zip)
+    array_files.inject(&:zip).map(&:flatten) # zipで縦横転置, flattenで配列を平坦化
   end
 end
 
@@ -19,7 +19,7 @@ def ls_output
   files_array.each do |files|
     unless_nil_file = files.compact # nil削除
     file_line = unless_nil_file.sum('') { |one_of_file| one_of_file.ljust(16) } # ファイル間隔統一
-    print file_line.rstrip
+    print file_line
     puts
   end
 end
